d583536b2f66269208014e976672c44e56ac4462
jieunko <cuytoeat@gmail.com>
2020-02-14 17:34:07 +0900

    sln file changed

--- FirstOpenCV/FirstOpenCV/FirstOpenCV.cpp
+++ FirstOpenCV/FirstOpenCV/FirstOpenCV.cpp
@@ -9,78 +9,14 @@ using namespace std;
 using namespace cv;
 using namespace chrono;
 
-class GridNode {
-public:
-	GridNode(int inten = 0, int c = 0) { intensity = inten; count = c; }
 
-public:
-	int intensity;
-	int count;
-};
-class BilateralGrid 
-{
-public:
-	int Intensity(int x, int y) { return SavedImage.at<uchar>(y, x); };
-	void SetImage(Mat Image) { cvtColor(Image, SavedImage, COLOR_BGR2GRAY); };
-	void FillGrid(int Spatial, int Range)
-	{
-		for (int i = 0; i < SavedImage.rows; i++) 
-		{
-			for (int j = 0; j < SavedImage.cols; j++)
-			{
-				int z = (int)(Intensity(i, j)/Range);
-				Grid[i / Spatial][j / Spatial][z].intensity += Intensity(i, j);
-				Grid[i / Spatial][j / Spatial][z].count += 1;
-			}
-		}
-	};
-
-	void GaussianConv(double SpatialSig, double RangeSig)
-	{
-	
-	}
-	
-private:
-	Mat SavedImage;
-	GridNode Grid[332][332][256];
-
-};
-
-//void initialize(GridNode arr[][1000][256], int block_x, int block_y=0, int block_z=0)
-//{
-//
-//   //#pragma omp parallel for 
-//	for (int i = 0; i < block_x; i++)
-//	{
-//		
-// 		for (int j = 0; j < block_y; j++) 
-//		{ 
-//		
-//			for (int k = 0; k < block_z; k++) 
-//			{
-//				
-//				arr[i][j][k].count = 0;
-//				arr[i][j][k].intensity = 0;
-//				
-//			}
-//		}
-//	}
-//
-//}
-//void filling(int grid[][1000][256], int Spatial, int Range)
-//{
-//
-//   
-//}
-
-GridNode grid[332][332][256];
 int main(int argc, char** argv) 
 {
 	
 	Mat src;
 	const int SpatialSamplingRate = 2;
 	const int RangeSamplingRate = 2;
-	BilateralGrid Bilateral;
+
 
 	system_clock::time_point start = system_clock::now();
 	//initialize(grid, 1000, 1000, 256); //1242*375
@@ -96,7 +32,6 @@ int main(int argc, char** argv)
 	}
 	cv::imshow("showTest", src);
 
-	printf("%d", grid[0][12][12].count);
 	
 	waitKey(0);
 
