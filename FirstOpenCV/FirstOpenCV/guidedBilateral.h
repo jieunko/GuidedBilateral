54d93a39ada9f4c9d91adad39dcc5952c0a3efcb
jieunko <cuytoeat@gmail.com>
2020-02-14 22:08:19 +0900

    noise model and gaussian function updated

--- FirstOpenCV/FirstOpenCV/guidedBilateral.h
+++ FirstOpenCV/FirstOpenCV/guidedBilateral.h
@@ -1,9 +1,27 @@
 #pragma once
 #include "main.h"
 #include <math.h>
-void guidedBilateralFilter(Mat& src, int kernelSize, double sigSpatial, double sigGuideI);
+void guidedBilateralFilter(Mat& GuideI, Mat& Src, Mat& Prev, int kernelSize, double sigSpatial, double sigGuideI);
 
 double calculateSEF(double noiseScaleT, double noiseShapeAlpha)
 {
 	return 1 / (2 * noiseShapeAlpha) * (pow((1 + noiseScaleT), noiseShapeAlpha) - 1);
 };
+
+double length(int x, int y, int x2, int y2)
+{
+	return sqrt(pow((x - x2), 2) + pow((y - y2), 2));
+
+}
+
+double calculateGaussian(double val, double sig)
+{
+	return exp(-(pow(val, 2)) / (2 * pow(sig, 2))) / (2 * CV_PI * pow(sig, 2));
+}
+
+double photometricNoiseModel(int x, int y, int x2, int y2, Mat& GuideI, double sigSpatial, double sigGuideI)
+{
+	double spatial = calculateGaussian(length(x, y, x2, y2), sigSpatial);
+	double guided = calculateGaussian(GuideI.at<uchar>(x, y) - GuideI.at<uchar>(x, y), sigGuideI);
+	return spatial * guided;
+}
